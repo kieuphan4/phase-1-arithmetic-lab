c0c597099f1cebba71ed094c124eabc41a5287b3
Kieu <kieuphan530@gmail.com>
2021-09-22 09:38:43 -0500

    commit tree 9b9b4aa785446b3b793c5bb1962361be9486ddca

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 16;
+let num4 = 4;
+const mod = num3 / num4;
+
+let max = Math.max(4, 7, 12, 15, 20);
